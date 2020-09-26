9944210c730e515b045ab76821798880f7107533
Dileep314 <n.dkumar314@gmail.com>
2020-09-26 15:02:13 +0530

    Updates

---
+++ precache-manifest.4456b6855a3921257a0a8345c1eed067.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ba721ecdc404862d72eac4795975c6",
+    "url": "/React-ToDo/index.html"
+  },
+  {
+    "revision": "c0eae70626855e7367ba",
+    "url": "/React-ToDo/static/css/main.5d408a16.chunk.css"
+  },
+  {
+    "revision": "c509ad99ada3bf458532",
+    "url": "/React-ToDo/static/js/2.ee9e9f8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDo/static/js/2.ee9e9f8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0eae70626855e7367ba",
+    "url": "/React-ToDo/static/js/main.275740e1.chunk.js"
+  },
+  {
+    "revision": "5d556d5c8941b40236a9",
+    "url": "/React-ToDo/static/js/runtime-main.485aeea0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-ToDo/static/media/logo.5d5d9eef.svg"
+  }
+]);
